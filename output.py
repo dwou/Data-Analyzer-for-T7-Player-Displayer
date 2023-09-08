6dc5133fe907559c74ac45efac3f745a030b8c98
dwou <111612803+dwou@users.noreply.github.com>
2023-09-08 17:51:40 -0400

    Update output.py
    various changes

--- output.py
+++ output.py
@@ -9,73 +9,83 @@ females = [i for i in charprops.keys() if charprops[i][0] == 1]
 dlcs    = [i for i in charprops.keys() if charprops[i][1] == 1]
 base_r  = [i for i in charprops.keys() if charprops[i][1] == 0]
 
+# Case insensitive
+comkeys = ["Aight","Okay","Good","Great","No skip","No re","Lose quit","Win quit","Cheat","Plug","Connection","Desync","No comment"]
+goods = ["Aight","Okay","Good","Great"]
 
 with open("Tekken Player List.txt","r") as f:
     lines = f.readlines()
 
+# Print character data line-by-line to verify groups
+def verify_groups(groups):
+    for i in groups:
+        print(f"{i[0]:30}{i[1]:12}{i[2]:18}{i[3]}")
 
 if 1: # analysis
     #                        Username     Char                ID         Comment
     groups = [ re.findall(r"^([^\t\n]+)\t+([a-zA-Z 7-]+?)\t+\((\d+)\)\t+?(.*)$",i)[-1] for i in lines ]
-
-    # Print character data line-by-line to verify groups
-    #for i in groups:
-    #    print(f"{i[0]:30}{i[1]:13}{i[2]:18}{i[3]}")
-    
-    names,chars,IDs,comments = [ [row[i].lower() for row in groups] for i in range(4)]
-    chars = [ i.title() for i in chars ]
+    if 0: verify_groups(groups)
+    names, chars, IDs, comments = [ [row[i] for row in groups] for i in range(4)]
+    comments = [ i.lower() for i in comments ]
     chars_nodupe = sorted([j for i,j in enumerate(chars) if j not in chars[i+1:]])
-    
-    comkeys = ["Aight","Okay","Good","Great","No skip","No re","Lose quit","Win quit","Cheat","Plug","Connection","Desync","No comment"]
-    goods = ["Aight","Okay","Good","Great"]
-    chars_map = {} # { Char : (picks,goods) }
 
-    # One ID with multiple entries
+
+    # Find IDs with multiple entries
     if (dupes := { j for i,j in enumerate(IDs) if j in IDs[i+1:] }):
-        print(f'Dupes: {dupes}')
+        print(f'Duplicate IDs: {dupes}')
     else:
         print('No duplicate IDs found in player list.')
+
     
     # Calculate frequency of each character and the frequency of their goodness
-    for i in range(len(groups)):
-        char,comment = chars[i], comments[i]
-        if char not in chars_map.keys():
-            chars_map[char] = [1,0]
-        else:
+    chars_map = {} # { Char : (picks,goods,no_comments) }
+    for char,comment in zip(chars,comments):
+        if char in chars_map:
             chars_map[char][0] += 1
-        for j in goods:
-            if j.lower() in comment:
-                chars_map[char][1] += 1
-    
-    
+        else:
+            chars_map[char] = [1,0,0]
+        if comment == 'no comment yet':
+            #print(f'No comment: {char}') # print chars with 'no comment yet'
+            chars_map[char][2] += 1
+        else:
+            for j in goods:
+                if j.lower() in comment:
+                    chars_map[char][1] += 1
+                    break
+
+
     # Calculate female, dlc set%
-    total,total_f,total_dlc = 0,0,0
+    total, total_f, total_dlc = 0, 0, 0
     for i in chars_map.keys():
         if charprops[i][1]: total_dlc += chars_map[i][0]
         if charprops[i][0]: total_f += chars_map[i][0]
         total += chars_map[i][0]
 
+
     # Print fun facts
-    print("")
+    print("\n  ~Fun Facts~\n")
     print("Total players VS'd:",total)
     print(f"({len(dlcs)}/15) DLCs found")
     print(f"({len(base_r)}/36) base rosters found")
     print(f"({len(females)}/18) females+Leo found")
     print(f"({len(males)}/33) males found")
-    print(f"{int(1000*total_f/total+.5)/10}% VS females (35.3% expected)")
-    print(f"{int(1000*total_dlc/total+.5)/10}% VS DLC (29.4% expected)")
+    print(f"{100*total_f/total:3.1f}% VS females (35.3% expected)")
+    print(f"{100*total_dlc/total:3.1f}% VS DLC (29.4% expected)")
     print(f"({sum([1 for i in range(len(chars)) if chars[i]=='Law' and 'connection' in comments[i]])}/{chars_map['Law'][0]}) Laws had a bad connection")
-    print(f"Cheaters/pluggers characters: {[chars[i] for i in range(len(chars)) if 'cheat' in comments[i] or 'plug' in comments[i]]}")
+    print(f"Cheaters/pluggers characters: {', '.join([chars[i] for i in range(len(chars)) if 'cheat' in comments[i] or 'plug' in comments[i]])}")
 
     # This function tacks on a sign to quickly see good%, from -- to ++
-    plus_minus_sign = lambda i: ['--','-','','+','++'][int((i[1][1]/i[1][0])*5-.000001)]
+    # Made to work with chars_map
+    plus_minus_sign = lambda i: ['--','-','','+','++'][int((i[1][1]/(i[1][0]-i[1][2]))*5-.000001)] if i[1][0] - i[1][2] > 0 else ''
     # Print character pickrate / good% etc.
-    print("\n\nGood%      Name          Freq")
+    print("\n  ~Chart~\n")
+    print("Note: Good% accounts for 'No comment', but freq does not\n")
+    print("Good%      Name          Freq")
     for i in sorted(chars_map.items(),key=(lambda x:x[1][0]))[::-1]:
-        print(f"({i[1][1]}/{i[1][0]}){(plus_minus_sign(i)).ljust(6)}{i[0].ljust(14)}{'*'*i[1][0]}")
-    print('')
+        print(f"({i[1][1]}/{i[1][0]-i[1][2]}){(plus_minus_sign(i)).ljust(6)}{i[0].ljust(14)}{'*'*i[1][0]}")
 
     # print comment frequencies
+    print("\n  ~Summary~\n")
     for j in comkeys:
         print(j,": ",len([i for i in comments if j.lower() in i]),sep="")
 
